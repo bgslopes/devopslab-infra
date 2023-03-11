63abb9c75f3d80c41e420436b85c7a1f1eb795fa
bgslopes <blopes9993@gmail.com>
2023-03-11 13:44:16 -0300

    Changes to be committed: 	modified:   gcp-main.tf 	modified:   variables.tf

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "my-repo" {
   location = var.region
-  repository_id = "devopslab-infra"
+  repository_id = "labDevopsImpacta"
   description = "Imagens Docker"
   format = "DOCKER"
 }
